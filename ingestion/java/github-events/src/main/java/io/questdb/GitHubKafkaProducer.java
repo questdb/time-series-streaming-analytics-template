7e7644d91cc4255ac0379dbba315ad2ce123da33
javier <javier@formatinternet.com>
2024-01-29 11:29:32 +0100

    Adding token from environment and removing created_at for the java ingestor

--- ingestion/java/github-events/src/main/java/io/questdb/GitHubKafkaProducer.java
+++ ingestion/java/github-events/src/main/java/io/questdb/GitHubKafkaProducer.java
@@ -12,7 +12,12 @@ import java.util.Properties;
 public class GitHubKafkaProducer {
 
     public static void main(String[] args) throws IOException {
-        String githubToken = "iTOKEN";  // Replace with your GitHub token
+        // Fetch GitHub token from environment variable
+        String githubToken = System.getenv("GITHUB_TOKEN");
+        if (githubToken == null || githubToken.isEmpty()) {
+            throw new IllegalStateException("GitHub token not found in environment variables.");
+        }
+
         String kafkaTopic = "github_events";       // Kafka topic to produce messages to
         String kafkaBroker = "localhost:9092";     // Kafka broker address
 
@@ -22,18 +27,23 @@ public class GitHubKafkaProducer {
         props.put("bootstrap.servers", kafkaBroker);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    
+
         try (Producer<String, String> producer = new KafkaProducer<>(props)) {
             while (true) {  // Loop to continuously fetch events
                 for (GHEventInfo event : github.getEvents()) {
-                    long createdAtMicro = event.getCreatedAt().getTime() * 1000;
-                    String message = String.format("{\"type\": \"%s\", \"repo\": \"%s\", \"actor\": \"%s\", \"created_at_microseconds\": %d}",
-                            event.getType(), event.getRepository().getName(), event.getActorLogin(), createdAtMicro);
+                    // Uncomment the following lines if you want to send the event timestamp 
+                    // rather than allow QuestDB to use the server timestamp
+                    // long createdAtMicro = event.getCreatedAt().getTime() * 1000;
+
+                    String message = String.format("{\"type\": \"%s\", \"repo\": \"%s\", \"actor\": \"%s\"}",
+                            event.getType(), event.getRepository().getName(), event.getActorLogin());
+                            // Uncomment and add the following inside the format method if using createdAtMicro
+                            // , \"created_at_microseconds\": %d", createdAtMicro
 
                     producer.send(new ProducerRecord<>(kafkaTopic, message));
                     System.out.println("Sent message: " + message);
                 }
-                
+
                 try {
                     Thread.sleep(10000); // Sleep for 10 seconds
                 } catch (InterruptedException e) {
